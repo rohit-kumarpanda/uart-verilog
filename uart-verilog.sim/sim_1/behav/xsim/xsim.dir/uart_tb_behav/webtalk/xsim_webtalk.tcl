f9f149448e6615af353728c948c46ff76ddfacc3
rohit-kumarpanda <rohitarts14@gmail.com>
2026-07-11 11:46:56 +0530

    Added testbench code and some debugging

--- uart-verilog.sim/sim_1/behav/xsim/xsim.dir/uart_tb_behav/webtalk/xsim_webtalk.tcl
+++ uart-verilog.sim/sim_1/behav/xsim/xsim.dir/uart_tb_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir C:/Users/rohit/VERILOG codes/uart-verilog/uart-verilog.sim/sim_1/behav/xsim/xsim.dir/uart_tb_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Sat Jul 11 00:50:32 2026" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Sat Jul 11 01:11:21 2026" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2018.3 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2405991" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "b1c5a494-931d-46b3-bb69-2a441423c60e" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "8215f4ba99cb44999c611d7b124f7a66" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "46" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "47" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Microsoft Windows 8 or later , 64-bit" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "AMD Ryzen 5 7535HS with Radeon Graphics        " -context "user_environment"
@@ -27,7 +27,7 @@ webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
 webtalk_add_data -client xsim -key runtime -value "3437720 ns" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "0" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.55_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "16284_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 3458018543 -regid "" -xml C:/Users/rohit/VERILOG codes/uart-verilog/uart-verilog.sim/sim_1/behav/xsim/xsim.dir/uart_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/rohit/VERILOG codes/uart-verilog/uart-verilog.sim/sim_1/behav/xsim/xsim.dir/uart_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/rohit/VERILOG codes/uart-verilog/uart-verilog.sim/sim_1/behav/xsim/xsim.dir/uart_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.50_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "16524_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 3630673082 -regid "" -xml C:/Users/rohit/VERILOG codes/uart-verilog/uart-verilog.sim/sim_1/behav/xsim/xsim.dir/uart_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/rohit/VERILOG codes/uart-verilog/uart-verilog.sim/sim_1/behav/xsim/xsim.dir/uart_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/rohit/VERILOG codes/uart-verilog/uart-verilog.sim/sim_1/behav/xsim/xsim.dir/uart_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
